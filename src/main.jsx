b4a682210d3d4c76a488e5b35edf0d686e36df7c
Shanjai_2006 <shanjaimurthi@gmail.com>
2024-11-02 11:38:14 +0530

    merchant

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Screen from './screens/screen5.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Screen/>
   </StrictMode>,
 )
